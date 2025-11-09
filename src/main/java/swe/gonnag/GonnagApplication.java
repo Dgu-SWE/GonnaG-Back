5d46baa24eb2bc60b99884f7ddd714a18756d1f9
sssuhyeon <ckc020506@gmail.com>
2025-11-09 20:15:54 +0900

    initial commit

---
+++ src/main/java/swe/gonnag/GonnagApplication.java
@@ -0,0 +1,13 @@
+package swe.gonnag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonnagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonnagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swe/gonnag/GonnagApplicationTests.java
@@ -0,0 +1,13 @@
+package swe.gonnag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonnagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

60f622c35c11297e921de6757c47b7bb2e455214
hys010730 <githwang01@gmail.com>
2025-11-28 00:55:41 +0900

    fix: Class, Program 엔티티 수정

--- src/main/java/swe/gonnag/repository/ProgramRepository.java
+++ src/main/java/swe/gonnag/repository/ProgramRepository.java
@@ -1,16 +1,25 @@
 package swe.gonnag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import swe.gonnag.domain.entity.ProgramEntity;
+
 import java.util.Optional;
 
 public interface ProgramRepository extends JpaRepository<ProgramEntity, Long> {
 
-    // 학과, 전공, 트랙, 기준연도 4가지 조건으로 조회 Department + Major + ProgramName + BaseYear
-    Optional<ProgramEntity> findByDMPB(
-            String departmentName,
-            String major,
-            String programName,
-            Integer baseYear
+    // 메서드 이름이 findByDMBP 이든 findByKimchi 이든 상관없습니다. @Query가 우선입니다.
+
+    @Query("SELECT p FROM ProgramEntity p " +
+            "WHERE p.departmentName = :dept " +
+            "AND p.major = :major " +
+            "AND p.baseYear = :year " +
+            "AND p.programName = :track")
+    Optional<ProgramEntity> findByDMBP(
+            @Param("dept") String departmentName,
+            @Param("major") String major,
+            @Param("year") Integer baseYear,
+            @Param("track") String programName
     );
 }
